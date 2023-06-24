54de6af6ef32ec3a7521d956cb4a21b6c60e2f90
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 16:47:44 +0000

    Correct d

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,16 +4,17 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        double eps = Double.Epsilon;
         if (a == 0)
         {
             throw new System.ArgumentException();
         }
         else
         {
-            double d = Math.Pow(b, 2);
-            double x_first = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
-            double x_second = c / x_first;
-            return new Double[2]{x_first, x_second};
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            double xFirst = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            double xSecond = c / xFirst;
+            return new Double[2]{xFirst, xSecond};
             //pepega
         }
         throw new NotImplementedException();
