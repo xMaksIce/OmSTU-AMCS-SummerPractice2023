56eb0ca4da078d7d54b17330d716a66c83c99132
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 17:51:43 +0000

    Проверка без d>0

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,7 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10, -6);
+        double eps = Math.Pow(10, -9);
         double d = Math.Pow(b, 2) - 4 * a * c;
         if (a == 0 || a == double.NaN || a == double.PositiveInfinity || a == double.NegativeInfinity
             || b == double.NaN || b == double.PositiveInfinity || b == double.NegativeInfinity
@@ -12,7 +12,6 @@ public class SquareEquation
         {
             throw new System.ArgumentException();
         }
-        // else if (Math.Abs(d) < eps)
         else if (Math.Abs(d) < eps)
         {
             double xFirst = -b / (2 * a);
@@ -24,6 +23,7 @@ public class SquareEquation
         }
         else
         {
+            return Array.Empty<double>(); // debug
             double xFirst = -(b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a);
             double xSecond = c / xFirst;
             return new Double[2]{xFirst, xSecond};
