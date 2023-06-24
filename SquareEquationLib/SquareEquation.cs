7f10d78ee2f8112d0b9652defccd59d216c72b44
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 17:41:40 +0000

    Эпсилон в -6

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,7 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10, -9);
+        double eps = Math.Pow(10, -6);
         double d = Math.Pow(b, 2) - 4 * a * c;
         if (a == 0 || a == double.NaN || a == double.PositiveInfinity || a == double.NegativeInfinity
             || b == double.NaN || b == double.PositiveInfinity || b == double.NegativeInfinity
@@ -13,7 +13,7 @@ public class SquareEquation
             throw new System.ArgumentException();
         }
         // else if (Math.Abs(d) < eps)
-        else if (d > -eps && d <= eps)
+        else if (Math.Abs(d) < eps)
         {
             double xFirst = -b / (2 * a);
             return new double[1] {xFirst};
