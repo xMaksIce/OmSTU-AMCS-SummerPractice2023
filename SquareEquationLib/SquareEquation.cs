fe32ddc3db2d30af7f8095d7b3b02e58e7d922a2
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 16:14:07 +0000

    First Equation Test

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,17 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
+        if (a == 0)
+        {
+            throw new System.ArgumentException();
+        }
+        else
+        {
+            double d = Math.Pow(b, 2);
+            double x_first = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            double x_second = c / x_first;
+            return new Double[2]{x_first, x_second};
+        }
         throw new NotImplementedException();
         // ATTENTION!!
     }
