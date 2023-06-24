5f54cc1d1444028a16b57384894a773909febb78
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 18:35:00 +0000

    Условия в норме, epsilon -2

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,7 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10, -9);
+        double eps = Math.Pow(10, -2);
         double d = Math.Pow(b, 2) - 4 * a * c;
         // 2 теста
         if (a == 0 || a == double.NaN || a == double.PositiveInfinity || a == double.NegativeInfinity
@@ -13,11 +13,11 @@ public class SquareEquation
         {
             throw new System.ArgumentException();
         }
-        /*else if (Math.Abs(d) < eps) // 1 тест
+        else if (Math.Abs(d) < eps) // 1 тест
         {
-            double xFirst = -b / 2;
+            double xFirst = -b / (2 * a);
             return new double[1] {xFirst};
-        }*/
+        }
         else if (d < 0) // 1 тест
         {
             return Array.Empty<double>();
