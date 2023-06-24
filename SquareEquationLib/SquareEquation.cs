c57234cdaee0c5a3e2e03958353b7e0b953a6259
xMaksIce <maksiceofficial@gmail.com>
2023-06-24 18:19:27 +0000

    Два корня с отриц. дискр.

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,7 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10, -4);
+        double eps = Math.Pow(10, -9);
         double d = Math.Pow(b, 2) - 4 * a * c;
         // 2 теста
         if (a == 0 || a == double.NaN || a == double.PositiveInfinity || a == double.NegativeInfinity
@@ -13,22 +13,12 @@ public class SquareEquation
         {
             throw new System.ArgumentException();
         }
-        else if (d < 0) // 1 тест
-        {
-            return Array.Empty<double>();
-        }
-        else if (Math.Abs(d) < eps) // 1 тест
-        {
-            double xFirst = -b / (2 * a);
-            return new double[1] {xFirst};
-        }
-        else // 2 теста
+        else if (d > -eps && d < 0)
         {
             double xFirst = -(b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a);
             double xSecond = c / xFirst;
             return new Double[2]{xFirst, xSecond};
         }
-        /*
         else if (Math.Abs(d) < eps) // 1 тест
         {
             double xFirst = -b / (2 * a);
@@ -43,6 +33,6 @@ public class SquareEquation
             double xFirst = -(b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a);
             double xSecond = c / xFirst;
             return new Double[2]{xFirst, xSecond};
-        }*/
+        }
     }
 }
