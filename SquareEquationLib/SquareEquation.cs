63e79366dbef4d52e3de3f727f9add6669ba2ae3
xMaksIce <maksiceofficial@gmail.com>
2023-06-25 17:06:44 +0000

    Финальная рабочая версия с проверкой a на погрешность

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,29 +4,28 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10, -2);
         double d = Math.Pow(b, 2) - 4 * a * c;
-        // 2 теста
-        if (a == 0 || a == double.NaN || a == double.PositiveInfinity || a == double.NegativeInfinity
-            || b == double.NaN || b == double.PositiveInfinity || b == double.NegativeInfinity
-            || c == double.NaN || c == double.PositiveInfinity || c == double.NegativeInfinity)
+        double eps = Math.Pow(10, -9);
+        if (Math.Abs(a) < eps || double.IsNaN(a) || double.IsInfinity(a) 
+            || double.IsNaN(b) || double.IsInfinity(b)
+            || double.IsNaN(c) || double.IsInfinity(c))
         {
             throw new System.ArgumentException();
         }
-        else if (Math.Abs(d) < eps) // 1 тест
+        else if (Math.Abs(d) < eps)
         {
             double xFirst = -b / (2 * a);
-            return new double[1] {xFirst};
+            return new double[1]{xFirst};
         }
-        else if (d < 0) // 1 тест
-        {
-            return Array.Empty<double>();
-        }
-        else // 2 теста
+        else if (d > 0)
         {
             double xFirst = -(b + Math.Sign(b) * Math.Sqrt(d)) / (2 * a);
             double xSecond = c / xFirst;
             return new Double[2]{xFirst, xSecond};
+        }
+        else
+        {
+            return Array.Empty<double>();
         }
     }
 }
