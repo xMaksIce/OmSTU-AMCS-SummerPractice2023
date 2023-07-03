844295e302f31e35f4e05e7078cf16dfe4e72588
xMaksIce <maksiceofficial@gmail.com>
2023-07-03 12:15:05 +0000

    Убрал перегрузку метода Move, исправил покрытие

--- spacebattleLib/spacebattleLib.cs
+++ spacebattleLib/spacebattleLib.cs
@@ -1,29 +1,14 @@
 ﻿namespace spacebattleLib;
-
 public class Spacebattle
 {
-    public static double[] Move(double[] shipCords, double[] speedCords)
-    {
-        return Movement(shipCords, speedCords);
-    }
     public static double[] Move(double[] shipCords, double[] speedCords, bool movementIsPossible)
-    {
-        if (movementIsPossible)
-        {
-            return Movement(shipCords, speedCords);
-        }
-        else 
-        {
-            throw new Exception();
-        }
-    }
-    private static double[] Movement(double[] shipCords, double[] speedCords)
     {
         for (int i = 0; i < 2; i++)
         {
             if (shipCords[i] == double.PositiveInfinity || speedCords[i] == double.PositiveInfinity || 
                 shipCords[i] == double.NegativeInfinity || speedCords[i] == double.NegativeInfinity ||
-                shipCords[i] == double.NaN || speedCords[i] == double.NaN)
+                shipCords[i] == double.NaN || speedCords[i] == double.NaN ||
+                !movementIsPossible)
                 {
                     throw new Exception();
                 }
