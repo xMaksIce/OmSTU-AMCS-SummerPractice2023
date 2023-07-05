86106edaa1a53989407cb73836578e06b5ca8ddb
xMaksIce <maksiceofficial@gmail.com>
2023-07-05 12:10:58 +0000

    d вычисляется после проверки аргументов

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,6 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double d = Math.Pow(b, 2) - 4 * a * c;
         double eps = Math.Pow(10, -9);
         if (Math.Abs(a) < eps || double.IsNaN(a) || double.IsInfinity(a) 
             || double.IsNaN(b) || double.IsInfinity(b)
@@ -12,7 +11,11 @@ public class SquareEquation
         {
             throw new System.ArgumentException();
         }
-        else if (Math.Abs(d) < eps)
+        else
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+        }
+        if (Math.Abs(d) < eps)
         {
             double xFirst = -b / (2 * a);
             return new double[1]{xFirst};
